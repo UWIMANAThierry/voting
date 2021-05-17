fd6fd62366c0f0bd74d0a984028f23c2b3a9dd47
seggi sms <seggimarugira@gmail.com>
2021-05-17 16:16:54 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,30 +36,7 @@ class MyApp extends StatelessWidget {
         ),
         debugShowCheckedModeBanner: false,
         home: HomePage());
-    // home: TakePicture());
-    // home: CandidateListPage());
-    // home: StartPage());
+   
   }
 }
 
-// Future<void> main() async {
-//   // Ensure that plugin services are initialized so that `availableCameras()`
-//   // can be called before `runApp()`
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // Obtain a list of the available cameras on the device.
-//   final cameras = await availableCameras();
-
-//   // Get a specific camera from the list of available cameras.
-//   final firstCamera = cameras.first;
-
-//   runApp(
-//     MaterialApp(
-//       theme: ThemeData.dark(),
-//       home: TakePicture(
-//         // Pass the appropriate camera to the TakePictureScreen widget.
-//         camera: firstCamera,
-//       ),
-//     ),
-//   );
-// }
